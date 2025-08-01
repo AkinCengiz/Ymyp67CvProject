bf17b9889ab3447287451105a46a4c6b1d02ca15
akincengiz <akincengiz@outlook.com.tr>
2025-08-01 21:45:54 +0300

    Manager classes started

---
+++ Ymyp67CvProject.Business/Concrete/SkillManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.UnitOfWorks;
+using Core.Utilities.Results;
+using Ymyp67CvProject.Business.Abstract;
+using Ymyp67CvProject.DataAccess.Abstract;
+using Ymyp67CvProject.Entity.Dtos.Skill;
+
+namespace Ymyp67CvProject.Business.Concrete;
+public class SkillManager : ISkillService
+{
+    private readonly ISkillRepository _skillRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public SkillManager(ISkillRepository skillRepository, IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _skillRepository = skillRepository;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public Task<IDataResult<SkillResponseDto>> AddAsync(SkillCreateRequestDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> UpdateAsync(SkillUpdateRequestDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> RemoveAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<SkillResponseDto>> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<IEnumerable<SkillResponseDto>>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<IEnumerable<SkillResponseDto>>> GetSkillsProgramLanguagesAsync(bool program)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<IEnumerable<SkillResponseDto>>> GetSkillsToolsAsync(bool tools)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Ymyp67CvProject.Business/Concrete/SocialAccountManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.UnitOfWorks;
+using Core.Utilities.Results;
+using Ymyp67CvProject.Business.Abstract;
+using Ymyp67CvProject.DataAccess.Abstract;
+using Ymyp67CvProject.Entity.Dtos.SocialAccount;
+
+namespace Ymyp67CvProject.Business.Concrete;
+public class SocialAccountManager : ISocialAccountService
+{
+    private readonly ISocialAccountRepository _socialAccountRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public SocialAccountManager(ISocialAccountRepository socialAccountRepository, IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _socialAccountRepository = socialAccountRepository;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public Task<IDataResult<SocialAccountResponseDto>> AddAsync(SocialAccountCreateRequestDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> UpdateAsync(SocialAccountUpdateRequestDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IResult> RemoveAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<SocialAccountResponseDto>> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<IEnumerable<SocialAccountResponseDto>>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<SocialAccountResponseDto>> GetSocialAccountByNameAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDataResult<IEnumerable<SocialAccountResponseDto>>> GetSocialAccountsByUserNameAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
